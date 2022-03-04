107d2cf514c87416a7454f1efeab2ccf3b1a8024
Micah Villaruz <micahmvillaruz@gmail.com>
2022-03-04 18:16:37 +0800

    Username Dropdown

--- customer-products.php
+++ customer-products.php
@@ -55,7 +55,6 @@ include("session.php");
                         <img class="img-fluid" src="assets/img/avatar.png" width="40" height="40" class="rounded-circle">
                             <span class="d-sm-inline d-none text-color ms-1">
                                 <?php
-                                    session_start();
                                     echo $_SESSION['username']; 
                                 ?>
                             </span>
@@ -70,6 +69,7 @@ include("session.php");
                             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"></path>
                         </svg>
                     </a>
+                    
                 </div>
             </nav>
         </div>
