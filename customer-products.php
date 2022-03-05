edaa8f90b5880e7931ddd250de4d22c3d8f3eb5b
Micah Villaruz <micahmvillaruz@gmail.com>
2022-03-05 11:13:18 +0800

    Fix Range

--- customer-products.php
+++ customer-products.php
@@ -12,10 +12,10 @@ include("session.php");
     <title>rxpress-main</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/alumuko/vanilla-datetimerange-picker@latest/dist/vanilla-datetimerange-picker.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 
 </head>
 
