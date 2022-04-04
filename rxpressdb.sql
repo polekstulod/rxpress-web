51db872742d086bd9b2dd588d95c11e129a8a456
Paul <paultulod@pm.me>
2022-04-04 21:36:11 +0800

    updated sql

--- rxpressdb.sql
+++ rxpressdb.sql
@@ -7,6 +7,9 @@
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
+CREATE DATABASE `rxpressdb`;
+USE `rxpressdb`;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
